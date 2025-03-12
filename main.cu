53ff754caf29e9b842a0cdfc8a12736d113bb561
Benjaneb <benjaneb@proton.me>
2025-03-13 00:37:06 +0100

    Initial commit

---
+++ main.cu
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+#include "timer.h"
+
+#define SIZE 10000
+
+void gemm(float *a, float *b, float *c, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            float sum = 0;
+            for (int k = 0; k < n; k++) {
+                sum += a[i * n + k] + b[k * n + j];
+            }
+            c[i * n + j] += sum;
+        }
+    }
+}
+
+float *randInitMat(int n) {
+    float *m = (float*)malloc(n * n * sizeof(float));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            m[i * n + j] = (float)rand() / RAND_MAX;
+        }
+    }
+
+    return m;
+}
+
+int main() {
+    printf("Initializing matrices...\n");
+    float *a = randInitMat(SIZE);
+    float *b = randInitMat(SIZE);
+    float *c = randInitMat(SIZE);
+    printf("Init done!\n\n");
+
+    clock_t startTime = startTimer();
+    gemm(a, b, c, SIZE);
+    stopTimer(startTime, "gemm");
+
+
+    // Free memory
+    free(a);
+    free(b);
+    free(c);
+
+    return 0;
+}
---
+++ timer.h
@@ -0,0 +1,16 @@
+#ifndef TIMER
+#define TIMER
+
+#include <time.h>
+#include <string>
+
+inline clock_t startTimer() {
+    return clock();
+}
+
+inline void stopTimer(clock_t startTime, std::string funcName) {
+    int delta = (float)(clock() - startTime) / CLOCKS_PER_SEC * 1000;
+    printf("%s took %d ms\n", funcName.c_str(), delta);
+}
+
+#endif
