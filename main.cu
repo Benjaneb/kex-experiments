7fea36336866acc70cb538d4939e8604a1e856ff
Benjaneb <benjaneb@proton.me>
2025-04-02 23:48:11 +0200

    Fix

--- main.cu
+++ main.cu
@@ -6,6 +6,10 @@
 
 #define SIZE 1000
 
+__global__ void gpu_gemm(float *a, float *b, float *c, int n) {
+
+}
+
 void gemm(float *a, float *b, float *c, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -13,7 +17,7 @@ void gemm(float *a, float *b, float *c, int n) {
             for (int k = 0; k < n; k++) {
                 sum += a[i * n + k] + b[k * n + j];
             }
-            c[i * n + j] += sum;
+            c[i * n + j] = sum;
         }
     }
 }
@@ -39,6 +43,16 @@ int main() {
 
     clock_t startTime = startTimer();
     gemm(a, b, c, SIZE);
+    stopTimer(startTime, "gemm");
+
+
+    float *d_a, *d_b, *d_c;
+    cudaMemcpy(d_a, a, SIZE * SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, SIZE * SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, c, SIZE * SIZE * sizeof(float), cudaMemcpyHostToDevice);
+
+    startTime = startTimer();
+    gpu_gemm(d_a, d_b, d_c, SIZE);
     stopTimer(startTime, "gemm");
 
 
